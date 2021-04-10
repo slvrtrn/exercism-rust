45f2e23fe5bf80f0b16cc03eba8bad7b2b36ed12
slvrtrn <hypnoash@gmail.com>
2021-04-10 11:57:47 +0200

    Prime Factors: it is much faster without is_prime check

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,22 +1,10 @@
-fn is_prime(x: &u64) -> bool {
-    let upper = ((*x as f64).sqrt() as u64) + 1;
-    for i in 2..upper {
-        if x % i == 0 {
-            return false;
-        }
-    }
-    return true;
-}
-
-
 pub fn factors(n: u64) -> Vec<u64> {
     if n < 2 {
         return vec![];
     }
-    let primes = (2..=n).filter(is_prime).into_iter();
     let mut result: Vec<u64> = vec![];
     let mut current = n;
-    for p in primes {
+    for p in 2..=n {
         loop {
             if current % p != 0 {
                 break;
