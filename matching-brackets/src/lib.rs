b38c341f1a8376691c37305118e7fddde638ef94
slvrtrn <hypnoash@gmail.com>
2021-04-11 20:37:04 +0200

    Matching Brackets

---
+++ matching-brackets/src/lib.rs
@@ -0,0 +1,20 @@
+pub fn brackets_are_balanced(string: &str) -> bool {
+    let mut expected_braces: Vec<char> = vec![];
+    for c in string.chars() {
+        match c {
+            '{' => expected_braces.push('}'),
+            '[' => expected_braces.push(']'),
+            '(' => expected_braces.push(')'),
+            c @ '}' | c @ ']' | c @ ')' => {
+                match expected_braces.last() {
+                    Some(ch) if *ch == c =>
+                        expected_braces.remove(expected_braces.len() - 1),
+                    _ =>
+                        return false
+                }
+            }
+            _ => {}
+        }
+    }
+    expected_braces.is_empty()
+}
