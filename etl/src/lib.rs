36e21827fbcce129c7a2f762182d65e3df3cc083
slvrtrn <hypnoash@gmail.com>
2021-04-15 20:06:39 +0200

    ETL

---
+++ etl/src/lib.rs
@@ -0,0 +1,17 @@
+use std::collections::BTreeMap;
+
+pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
+    let mut result: BTreeMap<char, i32> = BTreeMap::new();
+    for (k, v) in h.into_iter() {
+        for c in v.into_iter() {
+            let lowercase = c.to_ascii_lowercase();
+            result.insert(
+                lowercase,
+                result.get(&lowercase)
+                    .map(|x| x + k)
+                    .unwrap_or(*k),
+            );
+        }
+    }
+    result
+}
